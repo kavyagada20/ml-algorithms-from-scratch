c1fe35a5204ed6f4f25ea65f79b508a65e6d55ff
kavya <kavyagada20@gmail.com>
2026-03-17 22:50:07 +0530

    Added SARSA reinforcement learning implementation

---
+++ reinforcement_learning/sarsa/train.py
@@ -0,0 +1,112 @@
+"""
+Training script for SARSA agent.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from model import SARSA
+
+
+class GridWorld:
+    """
+    Simple GridWorld environment.
+    """
+
+    def __init__(self):
+
+        self.size = 5
+        self.goal = (4, 4)
+
+    def reset(self):
+
+        self.agent = [0, 0]
+        return self.get_state()
+
+    def get_state(self):
+
+        return self.agent[0] * self.size + self.agent[1]
+
+    def step(self, action):
+
+        if action == 0:
+            self.agent[0] = max(0, self.agent[0] - 1)
+
+        elif action == 1:
+            self.agent[0] = min(self.size - 1, self.agent[0] + 1)
+
+        elif action == 2:
+            self.agent[1] = max(0, self.agent[1] - 1)
+
+        elif action == 3:
+            self.agent[1] = min(self.size - 1, self.agent[1] + 1)
+
+        reward = -1
+        done = False
+
+        if tuple(self.agent) == self.goal:
+            reward = 10
+            done = True
+
+        return self.get_state(), reward, done
+
+
+env = GridWorld()
+
+state_size = env.size * env.size
+action_size = 4
+
+agent = SARSA(state_size, action_size)
+
+episodes = 300
+
+reward_history = []
+
+
+for episode in range(episodes):
+
+    state = env.reset()
+
+    action = agent.choose_action(state)
+
+    total_reward = 0
+
+    for step in range(50):
+
+        next_state, reward, done = env.step(action)
+
+        next_action = agent.choose_action(next_state)
+
+        agent.fit(state, action, reward, next_state, next_action, done)
+
+        state = next_state
+        action = next_action
+
+        total_reward += reward
+
+        if done:
+            break
+
+    agent.epsilon = max(0.05, agent.epsilon * 0.995)
+
+    reward_history.append(total_reward)
+
+    print(f"Episode {episode} Reward {total_reward}")
+
+
+plt.plot(reward_history)
+
+plt.title("SARSA Training Rewards")
+plt.xlabel("Episode")
+plt.ylabel("Reward")
+
+plt.savefig("training_rewards.png")
+
+plt.show()
+
+
+with open("metrics.txt", "w") as f:
+
+    f.write("SARSA Training Metrics\n")
+    f.write(f"Episodes: {episodes}\n")
+    f.write(f"Average Reward: {np.mean(reward_history)}\n")
